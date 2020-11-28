91dd7b132944d77c48c1793d758e6d52cb34ece1
Khanh <lkgame1993@gmail.com>
2020-11-28 14:26:49 +0700

    Initial Commit

---
+++ assignment2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  assignment2
+//
+//  Created by Khanh, Vo Tran Nhut on 11/28/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
